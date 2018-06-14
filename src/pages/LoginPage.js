060de76f9c28f7beb7a32ed0c4527483cd0f2884
Hans Poo <hans@welinux.cl>
2018-06-14 13:05:43 -0400

    Login page en camino

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { StyleProvider, Container, Text } from "native-base";
+import { StyleProvider, Container, Text, Button } from "native-base";
 import estilos from "../estilos";
 import getTheme from "../../native-base-theme/components";
 
 export default class LoginPage extends React.Component {
   render() {
     console.log("En login page");
+    const { navigation: { navigate } } = this.props;
     return (
       <StyleProvider style={getTheme()}>
         <Container style={estilos.centrado}>
           <Text>This is Content Section</Text>
+          <Button onPress={() => navigate("HomeNavigator")}>
+            <Text>Simulate successfull login</Text>
+          </Button>
         </Container>
       </StyleProvider>
     );
